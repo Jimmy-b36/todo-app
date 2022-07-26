34a31e3198c05c79e2262044808b2badaf0b85b2
James Ball <jamie.j.ball@gmail.com>
2022-07-26 13:26:23 -0700

    initial push

---
+++ components/ListContainer.tsx
@@ -0,0 +1,18 @@
+const ListContainer = () => {
+  return (
+    <div className="m-5">
+      <div className="flex justify-center">
+        <div>
+          <h1 className="text-4xl">List container</h1>
+          <ul className="list">
+            <li className="list-item">Item 1</li>
+            <li className="list-item">Item 2</li>
+            <li className="list-item">Item 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListContainer;
