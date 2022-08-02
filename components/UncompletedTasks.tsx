964b31fcf3c9488ce1db408cf5d41e7f5f5f417e
James Ball <jamie.j.ball@gmail.com>
2022-08-02 09:50:57 -0700

    typescript & tailwind

--- components/UncompletedTasks.tsx
+++ components/UncompletedTasks.tsx
@@ -1,11 +1,20 @@
-import { Key } from 'react';
+import { Tasks } from '@prisma/client';
+import React, { Key } from 'react';
 import { uncompletedTodos } from '../lib/taskSorter';
 
-const UncompletedTasks = ({ tasks, onChangeHandler }: any) => {
+interface IUncompletedTasksProps {
+  tasks: Array<Tasks>;
+  onChangeHandler: (e: React.ChangeEvent) => void;
+}
+
+const UncompletedTasks = ({
+  tasks,
+  onChangeHandler,
+}: IUncompletedTasksProps) => {
   return (
     <div className="flex justify-center">
-      <ul>
-        <h1 className="text-4xl">Todo: </h1>
+      <ul className="flex justify-center flex-col">
+        <h1 className="text-4xl flex justify-center">Todo: </h1>
         {uncompletedTodos(tasks)?.map(
           (
             todo: {
@@ -16,20 +25,22 @@ const UncompletedTasks = ({ tasks, onChangeHandler }: any) => {
             },
             index: Key
           ) => (
-            <div key={index} className="flex flex-row">
-              <h2 className="m-2 w-max">
-                {' '}
-                <strong>{todo.name}</strong>{' '}
-              </h2>
-              <p className="m-2">{todo.description}</p>
-              <input
-                type="checkbox"
-                name="todoComplete"
-                checked={todo.completed}
-                value={todo.id}
-                id="todoComplete"
-                onChange={onChangeHandler}
-              />
+            <div className="flex justify-center">
+              <div key={index} className="flex flex-row w-1/2">
+                <h2 className="m-2 w-full">
+                  {' '}
+                  <strong>{todo.name}</strong>{' '}
+                </h2>
+                <p className="m-2">{todo.description}</p>
+                <input
+                  type="checkbox"
+                  name="todoComplete"
+                  checked={todo.completed}
+                  value={todo.id}
+                  id="todoComplete"
+                  onChange={onChangeHandler}
+                />
+              </div>
             </div>
           )
         )}
