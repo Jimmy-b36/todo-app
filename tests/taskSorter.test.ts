91439817adbb4940ec28fe3889e7459fd0aa0cfd
James Ball <jamie.j.ball@gmail.com>
2022-08-04 16:18:45 -0700

    tests for task sorter functions

---
+++ tests/taskSorter.test.ts
@@ -0,0 +1,127 @@
+import { uncompletedTodos, completedTodos } from '../lib/taskSorter';
+import { Tasks } from '@prisma/client';
+
+const todos: Array<Tasks> = [
+  {
+    id: 1,
+    name: 'test1',
+    description: 'test1',
+    completed: false,
+    userId: 1,
+    createdAt: new Date('2022-08-04T23:13:08.248Z'),
+    updatedAt: new Date('2022-08-04T23:13:08.248Z'),
+  },
+  {
+    id: 2,
+    name: 'test2',
+    description: 'test2',
+    userId: 1,
+    completed: true,
+    createdAt: new Date('2022-08-04T23:13:08.248Z'),
+    updatedAt: new Date('2022-08-04T23:13:08.248Z'),
+  },
+  {
+    id: 3,
+    name: 'test3',
+    description: 'test3',
+    userId: 1,
+    completed: false,
+    createdAt: new Date('2022-08-04T23:13:08.248Z'),
+    updatedAt: new Date('2022-08-04T23:13:08.248Z'),
+  },
+  {
+    id: 4,
+    name: 'test4',
+    description: 'test4',
+    userId: 1,
+    completed: true,
+    createdAt: new Date('2022-08-04T23:13:08.248Z'),
+    updatedAt: new Date('2022-08-04T23:13:08.248Z'),
+  },
+];
+
+test('function should filter completed todos', () => {
+  expect(uncompletedTodos(todos)).toEqual([
+    {
+      id: 1,
+      name: 'test1',
+      description: 'test1',
+      completed: false,
+      userId: 1,
+      createdAt: new Date('2022-08-04T23:13:08.248Z'),
+      updatedAt: new Date('2022-08-04T23:13:08.248Z'),
+    },
+    {
+      id: 3,
+      name: 'test3',
+      description: 'test3',
+      userId: 1,
+      completed: false,
+      createdAt: new Date('2022-08-04T23:13:08.248Z'),
+      updatedAt: new Date('2022-08-04T23:13:08.248Z'),
+    },
+  ]);
+  expect(uncompletedTodos(todos)).not.toEqual([
+    {
+      id: 2,
+      name: 'test2',
+      description: 'test2',
+      userId: 1,
+      completed: true,
+      createdAt: new Date('2022-08-04T23:13:08.248Z'),
+      updatedAt: new Date('2022-08-04T23:13:08.248Z'),
+    },
+    {
+      id: 4,
+      name: 'test4',
+      description: 'test4',
+      userId: 1,
+      completed: true,
+      createdAt: new Date('2022-08-04T23:13:08.248Z'),
+      updatedAt: new Date('2022-08-04T23:13:08.248Z'),
+    },
+  ]);
+});
+
+test('function should filter out uncompleted todos', () => {
+  expect(completedTodos(todos)).toEqual([
+    {
+      id: 2,
+      name: 'test2',
+      description: 'test2',
+      userId: 1,
+      completed: true,
+      createdAt: new Date('2022-08-04T23:13:08.248Z'),
+      updatedAt: new Date('2022-08-04T23:13:08.248Z'),
+    },
+    {
+      id: 4,
+      name: 'test4',
+      description: 'test4',
+      userId: 1,
+      completed: true,
+      createdAt: new Date('2022-08-04T23:13:08.248Z'),
+      updatedAt: new Date('2022-08-04T23:13:08.248Z'),
+    },
+  ]);
+  expect(completedTodos(todos)).not.toEqual([
+    {
+      id: 1,
+      name: 'test1',
+      description: 'test1',
+      completed: false,
+      userId: 1,
+      createdAt: new Date('2022-08-04T23:13:08.248Z'),
+      updatedAt: new Date('2022-08-04T23:13:08.248Z'),
+    },
+    {
+      id: 3,
+      name: 'test3',
+      description: 'test3',
+      userId: 1,
+      completed: false,
+      createdAt: new Date('2022-08-04T23:13:08.248Z'),
+      updatedAt: new Date('2022-08-04T23:13:08.248Z'),
+    },
+  ]);
+});
