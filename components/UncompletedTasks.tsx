83bc12aedd271add7da10205341685fd8b54f64e
James Ball <jamie.j.ball@gmail.com>
2022-08-14 10:04:03 -0400

    change text to black

--- components/UncompletedTasks.tsx
+++ components/UncompletedTasks.tsx
@@ -14,7 +14,7 @@ const UncompletedTasks = ({
   return (
     <div className="flex justify-center">
       <ul className="flex justify-center flex-col">
-        <h1 className="text-4xl flex justify-center">Todo: </h1>
+        <h1 className="text-4xl flex justify-center text-black">Todo: </h1>
         {uncompletedTodos(tasks)?.map(
           (
             todo: {
@@ -26,12 +26,12 @@ const UncompletedTasks = ({
             index: Key
           ) => (
             <div className="flex justify-center">
-              <div key={index} className="flex flex-row w-1/2">
-                <h2 className="m-2 w-full">
+              <div key={index} className="flex flex-row w-3/4">
+                <h2 className="m-2 w-full text-black">
                   {' '}
                   <strong>{todo.name}</strong>{' '}
                 </h2>
-                <p className="m-2">{todo.description}</p>
+                <p className="m-2 text-black">{todo.description}</p>
                 <input
                   type="checkbox"
                   name="todoComplete"
